49e9f0ac2cd644c00c9f988326b49b76fabe417e
HCSolomon <hcyrilsolomon@gmail.com>
2019-09-27 02:06:34 -0700

    fixed request_builder

--- src/data-ingestion/build_requests.py
+++ src/data-ingestion/build_requests.py
@@ -4,19 +4,11 @@ import requests
 def api_request(json_info, ip_address, port):
     info = json.load(json_info)
     url = 'http://' + ip_address + ":" + port + "/inceptionV3/" + info["job_type"]
-    data = {
-        'api_dev_key': 'b442c74e8e250dec65e401712f82ff9a',
-        'api_option': 'paste',
-        }
-    info_headers = {
-        'Cache-Control': 'no-cache',
-        'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
-    }
     info_files = {
-        'train_bucket_name': info['bucket_name'],
-        'train_bucket_prefix': info['bucket_prefix']
+        'train_bucket_name':  info['bucket_name'],
+        'train_bucket_prefix':  info['bucket_prefix']
     }
-    response = requests.post(url, headers = info_headers, files = info_files)
+    response = requests.post(url, data = info_files)
 
     return response
 
