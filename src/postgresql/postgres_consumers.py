dce99133adbc3fdb7539c55162997426cc49deb0
Ubuntu <ubuntu@ip-10-0-0-13.us-west-2.compute.internal>
2019-10-02 07:39:44 +0000

    edited postgres_consumers

--- src/postgresql/postgres_consumers.py
+++ src/postgresql/postgres_consumers.py
@@ -2,9 +2,10 @@ import json
 import requests
 import flask
 from kafka import KafkaConsumer
+from postgres_helpers import consume_upsert
 
 
-KAFKA_BROKER = 'ec2-35-162-75-2.us-west-2.compute.amazonaws.com'
+KAFKA_BROKER = 'localhost'
 KAFKA_PORT = '9092'
 
 def postgres_consumer(topic, group_id):
@@ -13,3 +14,7 @@ def postgres_consumer(topic, group_id):
                             bootstrap_servers=[KAFKA_BROKER + ":" + KAFKA_PORT],
                             auto_offset_reset='earliest',
                             value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+
+    for message in consumer:
+        msg = message.value
+        id = consume_upsert(msg)
