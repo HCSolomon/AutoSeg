d234178bdb62469c14c3efed1e7ec77e14ed8e42
Ubuntu <ubuntu@ip-10-0-0-13.us-west-2.compute.internal>
2019-10-02 04:17:18 +0000

    edited postgres helpers and consumers

--- src/postgresql/postgres_consumers.py
+++ src/postgresql/postgres_consumers.py
@@ -7,14 +7,9 @@ from kafka import KafkaConsumer
 KAFKA_BROKER = 'ec2-35-162-75-2.us-west-2.compute.amazonaws.com'
 KAFKA_PORT = '9092'
 
-def sherlock_consumer(topic, group_id):
+def postgres_consumer(topic, group_id):
     consumer = KafkaConsumer(topic,
                             group_id=group_id, 
                             bootstrap_servers=[KAFKA_BROKER + ":" + KAFKA_PORT],
                             auto_offset_reset='earliest',
                             value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-
-    for message in consumer:
-        print(message.value)
-        request_info = message.value
-        api_request(request_info, 'localhost', '3031')
