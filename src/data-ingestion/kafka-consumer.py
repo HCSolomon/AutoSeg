14b8072c08b25200e5642764ee2c9b0ff1401c22
HCSolomon <hcyrilsolomon@gmail.com>
2019-10-01 18:54:45 -0700

    updated kafka-consumer.py

--- src/data-ingestion/kafka-consumer.py
+++ src/data-ingestion/kafka-consumer.py
@@ -5,7 +5,7 @@ from kafka import KafkaConsumer
 from time import sleep
 
 
-KAFKA_BROKER = 'localhost'
+KAFKA_BROKER = 'ec2-35-162-75-2.us-west-2.compute.amazonaws.com'
 KAFKA_PORT = '9092'
 
 
@@ -18,15 +18,15 @@ def api_request(json_info, ip_address, port):
     response = requests.post(url, data = info_files)
     return response
 
-consumer = KafkaConsumer('ml_job',
-                        group_id='sherlock', 
-                        bootstrap_servers=[KAFKA_BROKER + ":" + KAFKA_PORT],
-                        auto_offset_reset='earliest',
-                        value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+def sherlock_consumer(topic, group_id):
+    consumer = KafkaConsumer(topic,
+                            group_id=group_id, 
+                            bootstrap_servers=[KAFKA_BROKER + ":" + KAFKA_PORT],
+                            auto_offset_reset='earliest',
+                            value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
-
-for message in consumer:
-    print(message.value)
-    request_info = message.value
-    api_request(request_info, KAFKA_BROKER, '3031')
-    sleep(20)
+    for message in consumer:
+        print(message.value)
+        request_info = message.value
+        api_request(request_info, 'localhost', '3031')
+    
--- src/data-ingestion/kafka-producer.py
+++ src/data-ingestion/kafka-producer.py
@@ -2,19 +2,21 @@ import json
 import requests
 import flask
 from kafka import KafkaProducer
+from time import sleep
 
 
-KAFKA_BROKER = 'localhost'
+KAFKA_BROKER = 'ec2-35-162-75-2.us-west-2.compute.amazonaws.com'
 KAFKA_PORT = '9092'
 
-producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER + ":" + KAFKA_PORT])
-producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('utf-8'))
 
-data = {
-    'bucket_name': 'test-sherlock', 
-    'bucket_prefix': 'models/test-model',
-    'job_type': 'transfer'
-    }
+def sherlock_producer(bucket_name, imageset_name, bucket_prefix, job_type):
+    producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER + ":" + KAFKA_PORT],
+                        value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+    data = {
+        'bucket_name': bucket_name, 
+        'imageset_name': imageset_name,
+        'bucket_prefix': 'models/' + bucket_prefix,
+        'job_type': job_type
+        }
 
-for i in range(1):
-    producer.send('ml_job', data).get(timeout=20)
+    producer.send('ml_job', data).get(timeout=30)
