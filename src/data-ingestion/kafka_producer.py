54cde0dfca9a48ca314bd082a95c30d056a0768c
HCSolomon <hcyrilsolomon@gmail.com>
2019-10-05 23:25:47 -0700

    replaced imageset_name with model_name

--- src/data-ingestion/kafka_producer.py
+++ src/data-ingestion/kafka_producer.py
@@ -10,12 +10,12 @@ KAFKA_BROKER = 'ec2-35-162-75-2.us-west-2.compute.amazonaws.com'
 KAFKA_PORT = '9092'
 
 
-def sherlock_producer(bucket_name, imageset_name, bucket_prefix, job_type):
+def sherlock_producer(bucket_name, model_name, bucket_prefix, job_type):
     producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER + ":" + KAFKA_PORT],
                         value_serializer=lambda m: json.dumps(m).encode('utf-8'))
     data = {
         'bucket_name': bucket_name,
-        'imageset_name': imageset_name,
+        'model_name': model_name,
         'bucket_prefix': bucket_prefix,
         'job_type': job_type
         }
