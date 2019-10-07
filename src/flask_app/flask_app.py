2fc6f9038c2cf7151c570703e471017251d3cf8c
Ubuntu <ubuntu@ip-10-0-0-22.us-west-2.compute.internal>
2019-10-07 00:36:28 +0000

    added chart to flask_app

--- src/flask_app/flask_app.py
+++ src/flask_app/flask_app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, Markup, render_template
 
 app = Flask(__name__)
 
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
+
 @app.route('/')
 def homepage():
-    return "Hi there, I'm your future dashboard!"
+    pie_labels = labels
+    pie_values = values
+    return render_template('main.html', title='Class Composition of Current Dataset', max=17000, set=zip(values, labels, colors))    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
