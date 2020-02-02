e23de4639f3cd7c8e51b2b1b81023b80b57b7c3d
HCSolomon <hcyrilsolomon@gmail.com>
2020-02-02 14:22:46 -0800

    Added SherlockRetrain with Java API. Python version is incorporated into Sherlock.

--- src/main/java/SherlockRetrain.java
+++ src/main/java/SherlockRetrain.java
@@ -1,5 +1,3 @@
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import io.kubernetes.client.ApiException;
 
 import java.io.BufferedReader;
@@ -8,19 +6,19 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Paths;
-import java.util.List;
 
-public class SherlockTrain extends SherlockBase {
-    public SherlockTrain(String platform_ip, String port, String bucket_name, String model_pref, String model_name) throws IOException, ApiException {
+public class SherlockRetrain extends SherlockBase {
+    public SherlockRetrain(String platform_ip, String port, String bucket_name, String model_pref, String model_name) throws IOException, ApiException {
         super(platform_ip, port, bucket_name, model_pref, model_name);
     }
 
-    public void train() {
+    public void retrain() {
         try {
             URL url = new URL(getDataPrep().getKubernetesAPI().getLink());
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
+            connection.setRequestProperty("epochs", "5");
+            connection.setRequestProperty("batch_size", "30");
             connection.setRequestProperty("bucket_name", getDataPrep().getS3API().getData());
             connection.setRequestProperty("bucket_prefix", getModelPrefix());
             connection.setRequestProperty("model_name", getModelName());
@@ -45,19 +43,4 @@ public class SherlockTrain extends SherlockBase {
             e.printStackTrace();
         }
     }
-
-//    public void train() {
-//        for (List<S3ObjectSummary> images : getObjects()) {
-//            for (S3ObjectSummary os : images) {
-//                String path = os.getKey();
-//                String name = path.substring(path.lastIndexOf('/') + 1);
-//                String folder = path.substring(0, path.lastIndexOf('/'));
-//                String new_path = Paths.get(folder, "train", name).toString();
-//                AmazonS3 s3 = getDataPrep().getS3API().getS3();
-//                s3.copyObject(getDataPrep().getS3API().getData(), path,
-//                        getDataPrep().getS3API().getData(), new_path);
-//            }
-//        }
-//        System.out.println("** Completed training model. **");
-//    }
 }
